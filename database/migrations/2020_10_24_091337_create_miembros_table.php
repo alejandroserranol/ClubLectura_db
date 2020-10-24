42998db3a6e3520e201e04dbcf403d292090d486
Alejandro Serrano Loredo <alejandroserranol@hotmail.com>
2020-10-24 11:50:02 +0200

    En fase de micración BBDD

---
+++ database/migrations/2020_10_24_091337_create_miembros_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMiembrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('miembros', function (Blueprint $table) {
+            $table->string('dni', 11)->primary();
+            $table->string('nombre', 45)->nullable($value = true);
+            $table->string('apellido', 45)->nullable($value = true);
+            $table->string('email', 45)->unique()->nullable($value = false);
+            $table->string('telefono', 45)->nullable($value = true);
+            $table->string('direccion', 45)->nullable($value = true);
+            $table->tinyInteger('situacion_actual', 1)->default(1);
+            $table->date('fecha_alta')->nullable($value = false);
+            $table->date('fecha_baja')->nullable($value = true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('miembros');
+    }
+}
